8fdf823a4294b28f5628baa4deb9e0b46d228430
SherifAshraf98 <40580254+SherifAshraf98@users.noreply.github.com>
2020-07-04 18:18:42 +0200

    Fix bug in local connection

--- app.js
+++ app.js
@@ -142,10 +142,6 @@ app.post("/login", function(req, res) {
   })
 });
 
-let port = process.env.PORT;
-if (port === null || port === '') {
-  port = 3000;
-}
-app.listen(port, function(req, res) {
+app.listen(process.env.PORT || 3000, function(req, res) {
   console.log("Server has started successfully.");
 });
